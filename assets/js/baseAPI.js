e276cbd1cd05be4d124d741375e9d60ddcdfcd80
mahao <1772509168@qq.com>
2021-01-20 10:00:54 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -9,5 +9,24 @@ var baseURL = 'http://api-breakingnews-web.itheima.net'
 // 处理参数
 $.ajaxPrefilter(function(params){
     // 拼接对应环境的服务期地址
-    params.url = baseURL + params.url
+    params.url = baseURL + params.url;
+
+    // 身份验证
+    if(params.url.indexOf("/my/") !== -1){
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
+
+    // 拦截所有相应 判断身份认证信息
+    params.complete = function(res){
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if(obj.status == 1 && obj.message == '身份认证失败！'){
+             // 清空本地token
+             localStorage.removeItem('token');
+             // 页面跳转
+             location='/login.html';
+        }
+    }
 })
